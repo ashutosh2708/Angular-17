db5c50abde933f702957fb3acbcb91c31b463268
Ashutosh <ashutoshshinde1998@gmail.com>
2024-02-17 17:31:00 +0530

    form validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'medical-form',
+    loadChildren: () =>
+      import('./medical-form/medical-form.module').then(
+        (m) => m.MedicalFormModule
+      ),
+  },
+];
---
+++ src/app/medical-form/evaluation/evaluation.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+@Component({
+  selector: 'app-evaluation',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    CommonModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './evaluation.component.html',
+  styleUrl: './evaluation.component.scss',
+})
+export class EvaluationComponent {
+  medicalForm: any;
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.medicalForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern(/^.{1,50}$/)]],
+      age: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+      gender: ['', Validators.required],
+      address: ['', Validators.required],
+      diagnosis: ['', Validators.required],
+      contact: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      personalHistory: [''],
+      familyHistory: [''],
+      painScore: ['']
+    });
+  }
+  submitForm() {
+    if (this.medicalForm.valid) {
+      console.log("Form is submitted", this.medicalForm.value);
+
+    }
+  }
+
+}
